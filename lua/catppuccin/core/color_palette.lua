2e3e5053be8b5d62f1873f1197b5a8c5231174d3
adishourya <shourya.aditya10@gmail.com>
2022-05-26 11:05:09 +0530

    Almost there

--- lua/catppuccin/core/color_palette.lua
+++ lua/catppuccin/core/color_palette.lua
@@ -1,26 +1,29 @@
 local color_palette = {
+
 	rosewater = "#F5E0DC", -- Rosewater
-	flamingo = "#F2CDCD", -- Flamingo
-	mauve = "#DDB6F2", -- Mauve
-	pink = "#F5C2E7", -- Pink
-	red = "#F28FAD", -- Red
-	maroon = "#E8A2AF", -- Maroon
-	peach = "#F8BD96", -- Peach
-	yellow = "#FAE3B0", -- Yellow
-	green = "#ABE9B3", -- Green
-	blue = "#96CDFB", -- Blue
-	sky = "#89DCEB", -- Sky
-	teal = "#B5E8E0", -- Teal
-	lavender = "#C9CBFF", -- Lavender
-	white = "#D9E0EE", -- White
-	gray2 = "#C3BAC6", -- Gray2
-	gray1 = "#988BA2", -- Gray1
-	gray0 = "#6E6C7E", -- Gray0
-	black4 = "#575268", -- Black4
-	black3 = "#302D41", -- Black3
-	black2 = "#1E1E2E", -- Black2
-	black1 = "#1A1826", -- Black1
-	black0 = "#161320", -- Black0
+	flamingo = "#d08b65", -- Flamingo
+	mauve = "#998596", -- Mauve
+	maroon = "#d67f6f", -- maroon
+	red = "#c65f5f", -- Red
+	pink = "#E8A2AF", -- pink
+	peach = "#d9b27c", -- Peach
+	yellow = "#e9d38d", -- Yellow
+	green = "#859e82", -- Green
+	blue = "#728797", -- Blue
+	sky = "#788897", -- Sky
+	teal = "#879f82", -- Teal
+	lavender = "#998396", -- Lavender
+	white = "#d1c6b4", -- White
+
+	gray2 = "#cdc0ad", -- Gray2
+	gray1 = "#c8baa4", -- Gray1
+
+	gray0 = "#808080", -- Gray0
+	black4 = "#404040", -- Black3
+	black3 = "#252525", -- Black2 
+	black2 = "#181818", -- Black1
+	black1 = "#101010", -- Black1
+	black0 = "#000000", -- Black0
 }
 
 return color_palette
